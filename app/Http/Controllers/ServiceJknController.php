34f173b2a2a3959899613d901adc68a4f263df20
abdurrizqoarra <abdurrizqoaa@gmail.com>
2025-10-13 09:13:35 +0800

    perbaikin minor

--- app/Http/Controllers/ServiceJknController.php
+++ app/Http/Controllers/ServiceJknController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ServiceJknController extends Controller
 {
     public function taskId3()
     {
+        $now = Carbon::now(); // waktu saat ini
 
-        return response()->json(['message' => 'Task ID 3 executed successfully']);
+
+        $data = DB::table('referensi_mobilejkn_bpjs')
+            ->where('status', 'Checkin')
+            ->where('statuskirim', 'Belum')
+            ->whereDate('validasi', $now->toDateString())
+            ->get();
+
+        return response()->json(['message' => 'Data retrieved successfully', 'data' => $data], 200);
     }
 }
